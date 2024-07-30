930615e419748523d6e486532ef554878cb027ce
Keith Flynn <flynn.keith@gmail.com>
2024-07-29 23:11:43 -0400

    Add project files, WIP

---
+++ CatFood.cs
@@ -0,0 +1,10 @@
+﻿using PetShop;
+
+namespace PetStore
+{
+    internal class CatFood : Product
+    {
+        public double WeightPounds { get; set; }
+        public bool IsKittenFood { get; set; }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,16 @@
+﻿namespace PetShop
+{
+    internal class Product
+    {
+        public string Name { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; } = "";
+    }
+
+    public class DogLeash : Product
+    {
+        public int LengthInches { get; set; }
+        public string Material { get; set; } = "";
+    }
+}
