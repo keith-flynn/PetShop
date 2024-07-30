594e2d9b585b6d1ef61398f701b0d5ff3246ce34
Keith Flynn <flynn.keith@gmail.com>
2024-07-30 18:17:28 -0400

    Finished logic with Neil substitutions

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace PetStore
@@ -10,7 +11,35 @@ namespace PetStore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userInput = "";
+
+            while (userInput.ToLower() != "exit")
+            {
+                Console.WriteLine("Press 1 to add a product");
+                Console.WriteLine("Type 'exit' to quit");
+
+                userInput = Console.ReadLine() ?? "";
+
+                if (userInput == "1")
+                {
+                    CatFood catFood = new CatFood();
+
+                    Console.WriteLine("What is the name of the cat food?");
+                    catFood.Name = Console.ReadLine() ?? "";
+
+                    Console.WriteLine("What is the price of the cat food?");
+                    if (decimal.TryParse(Console.ReadLine() ?? "0", out decimal result))
+                    {
+                        catFood.Price = result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid price");
+                    }
+
+                    Console.WriteLine(JsonSerializer.Serialize(catFood));
+                }
+            }
         }
     }
 }
