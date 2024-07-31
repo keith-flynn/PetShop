d295b036e641deac48d8b7083b8d1a3e2419b6d3
Keith Flynn <flynn.keith@gmail.com>
2024-07-30 22:20:17 -0400

    Add inputs for quantity, description, and weight

--- Program.cs
+++ Program.cs
@@ -28,13 +28,36 @@ namespace PetStore
                     catFood.Name = Console.ReadLine() ?? "";
 
                     Console.WriteLine("What is the price of the cat food?");
-                    if (decimal.TryParse(Console.ReadLine() ?? "0", out decimal result))
+                    if (decimal.TryParse(Console.ReadLine() ?? "0", out decimal priceResult))
                     {
-                        catFood.Price = result;
+                        catFood.Price = priceResult;
                     }
                     else
                     {
                         Console.WriteLine("Invalid price");
+                    }
+
+                    Console.WriteLine("How many units to add?");
+                    if (int.TryParse(Console.ReadLine() ?? "0", out int quantityResult))
+                    {
+                        catFood.Quantity = quantityResult;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid quantity");
+                    }
+
+                    Console.WriteLine("Provide a brief product description:");
+                    catFood.Description = Console.ReadLine() ?? "";
+
+                    Console.WriteLine("What is the weight of a single unit? (in imperial units of measurement)");
+                    if (double.TryParse(Console.ReadLine() ?? "0", out double weightResult))
+                    {
+                        catFood.WeightPounds = weightResult;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid weight");
                     }
 
                     Console.WriteLine(JsonSerializer.Serialize(catFood));
